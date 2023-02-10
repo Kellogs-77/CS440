79141f831e04f9ff4a91c817c9c83815ce8ffaf0
spruhar <sr1525@scarletmail.rutgers.edu>
2023-02-10 17:07:06 -0500

    maze

--- hw1/MazeGenerator.py
+++ hw1/MazeGenerator.py
@@ -11,8 +11,8 @@ class MazeGenerator:
         weights=np.array([0.3, 0.7])
         items=np.array([1, 0])
         rng = np.random.default_rng()
-        array = rng.choice(items, size=100, p=weights / sum(weights))
-        array = np.reshape(array, (10, 10))
+        array = rng.choice(items, size=w*h, p=weights / sum(weights))
+        array = np.reshape(array, (w, h))
         array[0][0] = 0
         array[w-1][h-1] = 0
         return array
