69044cd11a379512d5395d92f355d69fb7cffef3
spruhar <sr1525@scarletmail.rutgers.edu>
2023-02-10 17:05:41 -0500

    maze

--- hw1/MazeGenerator.py
+++ hw1/MazeGenerator.py
@@ -7,8 +7,14 @@ class MazeGenerator:
         self.heaplist = [0][0]
 
     def create_maze(w, h):
-        array = np.random.randint(2, size=(w,h))
+
+        weights=np.array([0.3, 0.7])
+        items=np.array([1, 0])
+        rng = np.random.default_rng()
+        array = rng.choice(items, size=100, p=weights / sum(weights))
+        array = np.reshape(array, (10, 10))
         array[0][0] = 0
+        array[w-1][h-1] = 0
         return array
     
     array = create_maze(10,10)
