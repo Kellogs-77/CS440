c7b17e1248c5f949a454acbda9115174615a506e
Nihar Kella <testnihar99@gmail.com>
2023-02-10 16:27:05 -0500

    Adds functionality for inserting in BinaryHeap

--- hw1/BinaryHeap.py
+++ hw1/BinaryHeap.py
@@ -4,5 +4,18 @@ import Node
 
 class BinaryHeap:
     def __init__(self):
-        self.heapList = [0]
+        self.heapList = []
         self.currentSize = 0
+
+    def perc_up(heap_size):
+        while heap_size // 2 > 0:
+            if self.heapList[heap_size].f_val < self.heapList[heap_size // 2].f_val:
+                tmp = self.heapList[heap_size // 2]
+                self.heapList[heap_size // 2] = self.heapList[heap_size]
+                self.heapList[heap_size] = tmp
+            heap_size = heap_size // 2
+
+    def insert(n):
+        self.heapList.append(n)
+        self.currentSize = self.currentSize + 1
+        self.percUp(self.currentSize)
