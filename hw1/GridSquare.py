c6b5fd09f7f5d7fb9c7fab638f9151edd021707a
Nihar Kella <testnihar99@gmail.com>
2023-02-10 22:51:07 -0500

    Adds a way to represent squares in a grid

---
+++ hw1/GridSquare.py
@@ -0,0 +1,8 @@
+# GridSquare class, for all your GridSquare needs???
+from Node import Node
+
+
+class GridSquare:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+        self.node = Node()
