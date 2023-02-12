90b6bc524670351f5ead05d0ef4df798d597b47f
spruhar <sr1525@scarletmail.rutgers.edu>
2023-02-11 19:06:19 -0500

    repeated a fixes

--- hw1/RepeatedA.py
+++ hw1/RepeatedA.py
@@ -8,38 +8,49 @@ open_list = BinaryHeap()
 def heuristic(end_position, start_positon):
     return (abs(end_position[0] - start_positon[0]) + abs(end_position[1]-start_positon[1]))
 
+def check_blocked(cell):
+    if(maze[cell[0], cell[1]] == 1):
+        return True
+    return False
+
+def create_node(position, parent, g_val, h_val):
+    n = Node()
+    n.position = position
+    n.parents.insert(1, parent)
+    n.g_val = g_val
+    n.h_val = h_val
+    n.f_val = n.g_val + n.h_val
+    return n
+
 def RepeatedA(maze, start_state, target):
     #start the agent at the start state
     neighbors = [(0,1), (1,0), (0,-1), (-1,0)]
     agent_pos = start_state
 
     #set up the starting node
-    start = Node()
-    start.position = tuple(agent_pos)
-    start.g_val = 0
-    start.h_val = heuristic(target, agent_pos)
-    start.f_val = start.g_val + start.h_val
+    start = create_node(tuple(agent_pos), [], 0, heuristic(target, start_state))
+    
     #figure out if neighbors are blocked, if so, add them to the closed list
+    #if neighbors are open, put them in the open list
     for i,j in neighbors:
         if (agent_pos[0] + i >= 0) and (agent_pos[1] + j >= 0):
-            if maze[i][j] == 1:
+            if check_blocked((i,j)):
                 closed_list[(i, j)] = 0
             else:
-                n = Node()
-                n.position = (i,j)
-                n.parents.insert(1, start.position)
-                n.g_val = 1
-                n.h_val = heuristic(target, n.position)
-                n.f_val = n.g_val + n.h_val
+                n = create_node((i,j), start.position, 1, heuristic(target, (i,j)))
                 open_list.insert(n)
-
+    
+    closed_list[start.position] = 0
+    counter = 1
+    #set up the loop to continue until there is nothing in the open list  
     while(len(open_list.heap_list) > 1):
+
         min = open_list.delete_min()
 
-        closed_list[tuple(agent_pos)] = 0
+        closed_list[min.position] = 0
+        print(closed_list)
 
-
-    #set up the loop
+    
     
     if agent_pos != target:
         print("Cannot reach target")
