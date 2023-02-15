4f428482d4bac60a1d3d7763efeca39f0f67c80f
Nihar Kella <testnihar99@gmail.com>
2023-02-14 19:37:15 -0500

    Adds path checker and dummy grid generator

--- hw1/MazeGenerator.py
+++ hw1/MazeGenerator.py
@@ -1,22 +1,25 @@
 import numpy as np
 
+
 class MazeGenerator:
-    
 
     def __init__(self):
         self.heaplist = [0][0]
 
     def create_maze(w, h):
 
-        weights=np.array([0.3, 0.7])
-        items=np.array([1, 0])
+        weights = np.array([0.3, 0.7])
+        items = np.array([1, 0])
         rng = np.random.default_rng()
         array = rng.choice(items, size=w*h, p=weights / sum(weights))
         array = np.reshape(array, (w, h))
-        array[0][0] = 0        
+        array[0][0] = 0
         array[w-1][h-1] = 0
         empty_array = np.zeros((w, h))
         empty_array.fill(0)
         return array, empty_array
-    
-    
+
+    def create_dummy_grid(self, w, h):
+        grid = np.zeros((w, h))
+        grid.fill(0)
+        return grid
--- hw1/RepeatedA.py
+++ hw1/RepeatedA.py
@@ -4,20 +4,23 @@ from MazeGenerator import MazeGenerator
 
 closed_list = dict()
 open_list = BinaryHeap()
-path_list = dict() #stores neighbors that I can pick from to travel to
+path_list = dict()  # stores neighbors that I can pick from to travel to
 temporary_tree = []
 final_path = []
 maze = []
 empty_maze = []
 target = ()
 
+
 def heuristic(end_position, start_positon):
     return (abs(end_position[0] - start_positon[0]) + abs(end_position[1]-start_positon[1]))
 
+
 def check_blocked(cell):
-    if(maze[cell[0], cell[1]] == 1):
+    if (maze[cell[0], cell[1]] == 1):
         return True
     return False
+
 
 def create_node(position, parent, g_val, h_val):
     n = Node()
@@ -27,21 +30,25 @@ def create_node(position, parent, g_val, h_val):
     n.h_val = h_val
     return n
 
+
 def find_optimal_movement():
     min_val = min(path_list.values())
 
     for position in path_list:
         if path_list[position] == min_val:
             return position
-    
+
     return -1
+
 
 def check_in_bounds(tuple_to_check):
     return ((tuple_to_check[0] >= 0) and (tuple_to_check[1] >= 0)) and (tuple_to_check[0] < len(maze)) and (tuple_to_check[1] < len(maze))
 
+
 def possible_movement():
     val = (len(open_list.heap_list) > 0)
     return val
+
 
 def compounded_delete(position_check):
     min = open_list.delete_min()
@@ -49,78 +56,83 @@ def compounded_delete(position_check):
         open_list.insert(min)
         min = open_list.delete_min()
 
-#given a position, returns a list of neighbors
+# given a position, returns a list of neighbors
+
+
 def get_neighbors(position):
     list_of_neighbors = []
-    movements = [(0,1), (1,0), (0,-1), (-1,0)]
+    movements = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-    for i,j in movements:
+    for i, j in movements:
         possible_neighbor = (position[0] + i, position[1] + j)
         if check_in_bounds(possible_neighbor):
             list_of_neighbors.append(possible_neighbor)
 
     return list_of_neighbors
 
-#initializes nodes with all the positions and h values for the maze
+# initializes nodes with all the positions and h values for the maze
+
+
 def initialize_maze_nodes():
     maze_nodes = [[0]*len(maze) for i in range(len(maze))]
 
     for i in range(0, len(maze)):
 
         for j in range(0, len(maze)):
-            
-            n = create_node((i,j), (), 1000*100, heuristic(target, (i,j)))
+
+            n = create_node((i, j), (), 1000*100, heuristic(target, (i, j)))
             maze_nodes[i][j] = n
-    
+
     return maze_nodes
-    
+
+
 def traceback_path(maze_nodes, forward_or_back):
     final_path = []
     position = target
-    start_position = (0,0)
+    start_position = (0, 0)
 
     if forward_or_back == 1:
-        while(position != start_position):
+        while (position != start_position):
             final_path.append(position)
-            #set the position to the position of the nodes parent
+            # set the position to the position of the nodes parent
             position = maze_nodes[position[0]][position[1]].parents
-        
+
         final_path.append(start_position)
 
         for pos in reversed((final_path)):
             print(pos)
         return
-    
+
     elif forward_or_back == -1:
         position = start_position
-        while(position != target):
+        while (position != target):
             final_path.append(position)
-            #set the position to the position of the nodes parent
+            # set the position to the position of the nodes parent
             position = maze_nodes[position[0]][position[1]].parents
-        
+
         final_path.append(target)
 
         for pos in ((final_path)):
             print(pos)
 
 
-
-#given a start state and an end state, returns the shortest unblocked path from start to finish 
+# given a start state and an end state, returns the shortest unblocked path from start to finish
 def a_evaluator(current_node_position, maze_nodes):
-    #expand the node --> get a list of all the neighbors
+    # expand the node --> get a list of all the neighbors
     list_of_neighbors = get_neighbors(current_node_position)
-    #add unblocked neighbors to the open list
-    #add blocked neighbors to the closed list
-    for i,j in list_of_neighbors:
+    # add unblocked neighbors to the open list
+    # add blocked neighbors to the closed list
+    for i, j in list_of_neighbors:
         if maze[i][j] == 1:
-            closed_list[(i,j)] = 0
-        elif (maze[i][j] == 0) and ((i,j) not in closed_list):
-            open_list.update_node((i,j), current_node_position, maze_nodes)
-    #add the current node to the closed list
+            closed_list[(i, j)] = 0
+        elif (maze[i][j] == 0) and ((i, j) not in closed_list):
+            open_list.update_node((i, j), current_node_position, maze_nodes)
+    # add the current node to the closed list
     closed_list[current_node_position] = 0
 
-    return 
-    
+    return
+
+
 def forward_a(start_pos):
     maze_nodes = initialize_maze_nodes()
 
@@ -140,15 +152,16 @@ def forward_a(start_pos):
         min = open_list.delete_min()
         agent_pos = min.position
         a_evaluator(agent_pos, maze_nodes)
-    
+
     if agent_pos == target:
         traceback_path(maze_nodes, 1)
         return
-    
+
     if len(open_list.heap_list) == 1:
         print("There is no escape")
         return
-    
+
+
 def backward_a():
     maze_nodes = initialize_maze_nodes()
 
@@ -168,20 +181,37 @@ def backward_a():
         min = open_list.delete_min()
         agent_pos = min.position
         a_evaluator(agent_pos, maze_nodes)
-    
+
     if agent_pos == start_node.position:
         traceback_path(maze_nodes, -1)
         return
-    
+
     if len(open_list.heap_list) == 1:
         print("There is no escape")
         return
 
 
+def check_path(maze, path):
+    output = []
+
+    # check if every position in the path is a '0'
+    for position in path:
+        if (maze[position[0]][position[1]] == 1):
+
+            # if there's a '1' False and the position so it can update forward_a's grid
+            output = [False, (position[0], position[1])]
+            return output
+        else:
+            continue
+    # if not, then the path is good!
+    output = [True]
+    return output
+
+
 if __name__ == "__main__":
-    #create the initial maze
-    maze, empty_maze = MazeGenerator.create_maze(5,5)
+    # create the initial maze
+    maze, empty_maze = MazeGenerator.create_maze(5, 5)
     target = (len(maze) - 1, len(maze) - 1)
     print(maze)
-    forward_a((0,0))
-    #backward_a()
+    forward_a((0, 0))
+    # backward_a()
